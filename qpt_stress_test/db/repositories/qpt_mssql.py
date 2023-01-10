f85ccb8e044926a8d768c5c5e91f1d1a78fc6354
galaxy-sk <100124473+galaxy-sk@users.noreply.github.com>
2023-01-10 14:23:22 +0000

    fix: add bank balances to bovas recon (#8)

--- qpt_stress_test/db/repositories/qpt_mssql.py
+++ qpt_stress_test/db/repositories/qpt_mssql.py
@@ -47,6 +47,13 @@ class OperationsRepository:
 
 GET_TRADING_BALANCES_LAST_TRADEDATE = "SELECT Max(TradeDate) as last_date FROM trading.v2.EndOfDayBalance;"
 
+# crypto.bankbalances: ['ID', 'api_name', 'bfc_name', 'currency', 'balance', 'trade_date', 'InsertTime', 'source']
+GET_CRYPTO_BANKBALANCES = """
+    select bfc_name as Account, balance as Balance, 'Cash' as BalanceType, UPPER(currency) as Currency,
+        'crypto.bankbalances' as Source, trade_date as Timestamp, InsertTime as Timestamp_Native
+    from trading.crypto.bankbalances 
+    where trade_date = '{eod_date:%Y%m%d}'
+"""
 
 GET_CME_POSITIONS = """
     SELECT 
@@ -87,94 +94,85 @@ GET_CME_POSITIONS = """
     WHERE p.Position <> 0  AND p.Account in {accounts} and convert(DATE, m.TradeDate) = '{as_of:%Y-%m-%d}'
 """
 
+# trading.pnl.EOD_NEW positions
+# Trading balances by client - swaps and futures look to match out against actual positions, if you remove symbols that we don't have positions in.
+# But there seems to be no easy way of working out which coin balances are current or stale
+# Most likely, we would need to get valide Client/Account/Symbol values from some other source to be usable by risk
 """
-DECLARE @T_DATE AS DATE; SET @T_DATE = '20221129';
-
-	SELECT DISTINCT SecurityExchange as ex_endpoint, BFCSymbol as symbol_bfc, ContractMultiplier AS qty_multiplier, NULL AS lot_Size, Symbol as symbol_exch, NULL AS currency_position, 
-	          NULL AS currency_quote, NULL AS IsInverse, SecurityType AS Inst_Type 
-	FROM [Securities].dbo.CryptoSecurities WITH(NOLOCK)
-	WHERE is_active = 1 AND BFCSymbol in (SELECT distinct symbol FROM trading.crypto.EndOfDay WITH(NOLOCK) WHERE CONVERT(DATE,TradeDate) = @T_DATE) 
-	ORDER BY  ex_endpoint, symbol_bfc;
-
-	SELECT DISTINCT 
-		CASE WHEN exchange = 'BINANCE' THEN 'BINE' WHEN EXCHANGE = 'Huobi' OR exchange = 'HUOBI' THEN 'HUBI' WHEN exchange = 'Kraken' THEN 'KRKN' ELSE exchange END as ex_endpoint, 
-			bfcSymbol as symbol_bfc, Multiplier AS qty_multiplier, lotSize AS lot_Size, exchSymbol as symbol_exch, positionCurrency AS currency_position, rootsymbol ,
-	           Quotecurrency AS currency_quote, IsInverse, Expiry, class AS Inst_Type
-	FROM [Securities].dbo.CryptoFutures WITH(NOLOCK)
-	WHERE is_active = 1 AND Expiry >  @T_DATE AND BFCSymbol in (SELECT distinct symbol FROM trading.crypto.EndOfDay WITH(NOLOCK) WHERE CONVERT(DATE,TradeDate) = @T_DATE) 
-	ORDER BY ex_endpoint, symbol_bfc;
+	SELECT CONVERT(DATE,YYYYMMDD) AS tradedate, Client, endpoint, Account, eod.Symbol, 
+		MAX(CASE WHEN [TYPE] = 'BALANCE' THEN product ELSE '' END ) AS eodposition_currency,
+		MAX(CASE WHEN [TYPE] = 'PNL' THEN product ELSE '' END ) AS pnl_currency,
+		sum(CASE WHEN [TYPE] = 'BALANCE' THEN value ELSE 0 END) AS eodposition,
+		sum(CASE WHEN [TYPE] = 'PNL' THEN value ELSE 0 END) AS pnl,
+		sum(CASE WHEN [TYPE] = 'FEE' THEN value ELSE 0 END) AS fee,
+		sum(CASE WHEN [TYPE] = 'DELTA_OFFSET' THEN value ELSE 0 END) AS delta_offset,
+		sum(CASE WHEN [TYPE] = 'FUNDING' THEN value ELSE 0 END) AS funding,
+		sum(CASE WHEN [TYPE] = 'QUANTITYxPRICE' THEN value ELSE 0 END) AS quantity_by_price,
+		sum(CASE WHEN [TYPE] = 'QUANTITY/PRICE' THEN value ELSE 0 END) AS quantity_over_price
+	FROM trading.pnl.EOD_NEW eod WITH(NOLOCK)
+	WHERE YYYYMMDD = '{eod_date:%Y%m%d}' 
+		AND (Account in (SELECT Account FROM Operations.balances.EndOfDay_00UTC WHERE balance <> 0  and Date_UTC = @T_DATE ) 
+		      OR Endpoint in ('FEE', 'FUND', 'LEND', 'REBATE', 'REWARD', 'YIELD', 'CME'))--  AND Symbol in ('BLINKUSDT', 'O_P_LINK-USDT-SWAP', 'LINK', 'LINKUSD', 'LINKUSDT') AND Account in ('BINE-2-S1-E')
+	GROUP BY YYYYMMDD, endpoint, Client, Account, Symbol
+	HAVING NOT(abs(sum(CASE WHEN [TYPE] = 'BALANCE' THEN value ELSE 0 END)) <= 1e-08) 
+	ORDER BY Client, account, symbol
 """
-
-# Client Data
 """
-SELECT 
-    CONVERT(DATE,YYYYMMDD) AS tradedate, 
-    Client, 
-    Account, 
-    endpoint, 
-    eod.Symbol, 
-    CASE WHEN crypto_inst.Inst_Type IS NOT NULL THEN crypto_inst.Inst_Type WHEN cme_inst.SecurityType IS NOT NULL THEN cme_inst.SecurityType ELSE '' END AS instrument_type, 
-    CASE WHEN crypto_inst.Expiry IS NOT NULL THEN crypto_inst.Expiry  WHEN  cme_inst.Expiration IS NOT NULL THEN cme_inst.Expiration ELSE NULL END AS expiry, 
-    CASE WHEN crypto_inst.IsInverse IS NOT NULL THEN crypto_inst.IsInverse  ELSE 0 END AS is_inverse, 
-    CASE WHEN cme_inst.currency_position IS NOT NULL THEN cme_inst.currency_position
-         WHEN crypto_inst.currency_position IS NOT NULL AND crypto_inst.currency_position <> '' THEN crypto_inst.currency_position 
-         WHEN crypto_inst.rootsymbol IS NOT NULL THEN crypto_inst.rootsymbol 
-         ELSE MAX(CASE WHEN [TYPE] = 'BALANCE' THEN product ELSE '' END) END AS inst_position_currency,
-    marks.closingprice,
-    txn_count,	
-    today_short_quantity,
-    today_long_quantity, 
-    sum(CASE WHEN [TYPE] = 'BALANCE' THEN value ELSE 0 END) AS eodposition,
-    MAX(CASE WHEN [TYPE] = 'BALANCE' THEN product ELSE '' END ) AS eodposition_currency,
-    sum(CASE WHEN [TYPE] = 'PNL' THEN value ELSE 0 END) AS pnl,
-    MAX(CASE WHEN [TYPE] = 'PNL' THEN product ELSE '' END ) AS pnl_currency,
-    sum(CASE WHEN [TYPE] = 'FEE' THEN value ELSE 0 END) AS fee,
-    sum(CASE WHEN [TYPE] = 'DELTA_OFFSET' THEN value ELSE 0 END) AS delta_offset,
-    sum(CASE WHEN [TYPE] = 'FUNDING' THEN value ELSE 0 END) AS funding,
-    sum(CASE WHEN [TYPE] = 'QUANTITYxPRICE' THEN value ELSE 0 END) AS quantity_by_price,
-    sum(CASE WHEN [TYPE] = 'QUANTITY/PRICE' THEN value ELSE 0 END) AS quantity_over_price,
-    isnull(marks.closingprice, 1) * sum(CASE WHEN [TYPE] = 'BALANCE' THEN value ELSE 0 END) AS notional 
-FROM trading.pnl.EOD_NEW eod WITH(NOLOCK)
-  LEFT JOIN  (
-    SELECT DISTINCT 
-        CASE WHEN exchange = 'BINANCE' THEN 'BINE' 
-             WHEN EXCHANGE = 'Huobi' OR exchange = 'HUOBI' THEN 'HUBI' 
-             WHEN exchange = 'Kraken' THEN 'KRKN' 
-             ELSE exchange END as ex_endpoint, 
-        bfcSymbol as symbol_bfc, 
-        Multiplier AS qty_multiplier, 
-        lotSize AS lot_Size, 
-        exchSymbol as symbol_exch, 
-        rootsymbol,
-        positionCurrency AS currency_position,
-        Quotecurrency AS currency_quote,
-        IsInverse, 
-        class AS Inst_Type, 
-        Expiry
-    FROM [Securities].dbo.CryptoFutures WITH(NOLOCK)
-    WHERE is_active = 1 ) crypto_inst ON eod.Symbol = crypto_inst.symbol_bfc AND eod.endpoint = ex_endpoint
-  LEFT JOIN (
-    SELECT YYYYMMDD as txn_YYYYMMDD, client as txn_client, ACCOUNT as txn_ACCOUNT, endpoint as txn_endpoint, symbol as txn_symbol, 
-        count(distinct tid) AS txn_count, 
-        SUM(LastFillQuantity * CASE SIDE WHEN 'S' THEN -1 ELSE 0 END) / 1000000000.0 as today_short_quantity,
-        SUM(LastFillQuantity * CASE SIDE WHEN 'S' THEN 0 ELSE 1 END) / 1000000000.0 as today_long_quantity
-    FROM trading.crypto.backfill tx WITH(NOLOCK)
-    WHERE YYYYMMDD = '{0:%Y%m%d}'
-    GROUP BY YYYYMMDD, client, ACCOUNT, endpoint, symbol) txns ON txn_YYYYMMDD = eod.YYYYMMDD AND txn_client = client AND  txn_ACCOUNT = account and txn_endpoint = endpoint AND txn_symbol = eod.symbol
-  LEFT JOIN  (
-    SELECT [Contract] AS symbol_bfc, Exchange, Multiplier, SecurityType, Expiration, Product AS currency_position, CurrencyCode AS currency_quote
-    FROM Trading.dbo.products WITH(NOLOCK)
-    WHERE Exchange = 'CME' AND SecurityType = 'FUTURE') cme_inst ON eod.Symbol = cme_inst.symbol_bfc
-  LEFT JOIN (
-    SELECT Symbol As mark_symbol, TradeDate as mark_date, avg(ClosingPrice) as closingprice
-    FROM trading.crypto.marksettle WITH(NOLOCK)
-    GROUP BY Symbol, TradeDate) marks ON marks.mark_symbol = eod.symbol AND marks.mark_date = YYYYMMDD
- WHERE YYYYMMDD = '{0:%Y%m%d}'  AND (crypto_inst.Expiry > '{0:%Y%m%d}' OR cme_inst.Expiration > '{0:%Y%m%d}' OR (crypto_inst.Expiry is null and cme_inst.Expiration is null))
- GROUP BY YYYYMMDD, endpoint, Client, Account, Symbol, crypto_inst.Expiry, cme_inst.Expiration, crypto_inst.rootsymbol, cme_inst.currency_position, crypto_inst.IsInverse, 
-    crypto_inst.currency_position, crypto_inst.Inst_Type, cme_inst.SecurityType, marks.closingprice, txn_count,	today_short_quantity, today_long_quantity
- HAVING 
-    NOT(abs(sum(CASE WHEN [TYPE] = 'BALANCE' THEN value ELSE 0 END)) <= 1e-08)
- ORDER BY YYYYMMDD, Client, endpoint, Account, Symbol;
+	SELECT CONVERT(DATE,YYYYMMDD) AS tradedate, Client, endpoint, Account, eod.Symbol, 
+		txn_count,	
+		CASE WHEN crypto_inst.Inst_Type IS NOT NULL THEN crypto_inst.Inst_Type WHEN cme_inst.SecurityType IS NOT NULL THEN cme_inst.SecurityType ELSE '' END AS instrument_type, 
+		CASE WHEN crypto_inst.Expiry IS NOT NULL THEN crypto_inst.Expiry  WHEN  cme_inst.Expiration IS NOT NULL THEN cme_inst.Expiration ELSE NULL END AS expiry, 
+		CASE WHEN crypto_inst.IsInverse IS NOT NULL THEN crypto_inst.IsInverse  ELSE 0 END AS is_inverse, 
+		CASE WHEN cme_inst.currency_position IS NOT NULL THEN cme_inst.currency_position
+		     WHEN crypto_inst.currency_position IS NOT NULL AND crypto_inst.currency_position <> '' THEN crypto_inst.currency_position 
+		     WHEN crypto_inst.rootsymbol IS NOT NULL THEN crypto_inst.rootsymbol 
+			 ELSE MAX(CASE WHEN [TYPE] = 'BALANCE' THEN product ELSE '' END) END AS inst_position_currency,
+		marks.closingprice,
+		today_short_quantity,
+		today_long_quantity,
+		sum(CASE WHEN [TYPE] = 'BALANCE' THEN value ELSE 0 END) AS eodposition,
+		MAX(CASE WHEN [TYPE] = 'BALANCE' THEN product ELSE '' END ) AS eodposition_currency,
+		sum(CASE WHEN [TYPE] = 'PNL' THEN value ELSE 0 END) AS pnl,
+		MAX(CASE WHEN [TYPE] = 'PNL' THEN product ELSE '' END ) AS pnl_currency,
+		sum(CASE WHEN [TYPE] = 'FEE' THEN value ELSE 0 END) AS fee,
+		sum(CASE WHEN [TYPE] = 'DELTA_OFFSET' THEN value ELSE 0 END) AS delta_offset,
+		sum(CASE WHEN [TYPE] = 'FUNDING' THEN value ELSE 0 END) AS funding,
+		sum(CASE WHEN [TYPE] = 'QUANTITYxPRICE' THEN value ELSE 0 END) AS quantity_by_price,
+		sum(CASE WHEN [TYPE] = 'QUANTITY/PRICE' THEN value ELSE 0 END) AS quantity_over_price,
+		isnull(marks.closingprice, 1) * sum(CASE WHEN [TYPE] = 'BALANCE' THEN value ELSE 0 END) AS notional
+	FROM trading.pnl.EOD_NEW eod WITH(NOLOCK)
+	  LEFT JOIN  (
+		SELECT DISTINCT 
+			CASE WHEN exchange = 'BINANCE' THEN 'BINE' WHEN EXCHANGE = 'Huobi' OR exchange = 'HUOBI' THEN 'HUBI' WHEN exchange = 'Kraken' THEN 'KRKN' ELSE exchange END as ex_endpoint, 
+			bfcSymbol as symbol_bfc, Multiplier AS qty_multiplier, lotSize AS lot_Size, exchSymbol as symbol_exch, rootsymbol,
+			positionCurrency AS currency_position,
+			Quotecurrency AS currency_quote,
+			IsInverse, class AS Inst_Type , Expiry
+		FROM [Securities].dbo.CryptoFutures WITH(NOLOCK)
+		WHERE is_active = 1 ) crypto_inst ON eod.Symbol = crypto_inst.symbol_bfc AND eod.endpoint = ex_endpoint
+	  LEFT JOIN (
+		SELECT YYYYMMDD as txn_YYYYMMDD, client as txn_client, ACCOUNT as txn_ACCOUNT, endpoint as txn_endpoint, symbol as txn_symbol, 
+			count(distinct tid) AS txn_count, 
+			SUM(LastFillQuantity * CASE SIDE WHEN 'S' THEN -1 ELSE 0 END) / 1000000000.0 as today_short_quantity,
+			SUM(LastFillQuantity * CASE SIDE WHEN 'S' THEN 0 ELSE 1 END) / 1000000000.0 as today_long_quantity
+		FROM trading.crypto.backfill tx WITH(NOLOCK)
+		WHERE YYYYMMDD = @T_DATE
+		GROUP BY YYYYMMDD, client, ACCOUNT, endpoint, symbol
+	  ) txns ON txn_YYYYMMDD = eod.YYYYMMDD AND txn_client = client AND  txn_ACCOUNT = account and txn_endpoint = endpoint AND txn_symbol = eod.symbol
+	  LEFT JOIN  (
+		SELECT [Contract] AS symbol_bfc, Exchange, Multiplier, SecurityType, Expiration, Product AS currency_position, CurrencyCode AS currency_quote
+		FROM Trading.dbo.products WITH(NOLOCK)
+		WHERE Exchange = 'CME' AND SecurityType = 'FUTURE') cme_inst ON eod.Symbol = cme_inst.symbol_bfc
+	  LEFT JOIN (
+		SELECT Symbol As mark_symbol, TradeDate as mark_date, avg(ClosingPrice) as closingprice
+		FROM trading.crypto.marksettle WITH(NOLOCK)
+		GROUP BY Symbol, TradeDate) marks ON marks.mark_symbol = eod.symbol AND marks.mark_date = YYYYMMDD
+	 WHERE YYYYMMDD = @T_DATE  AND (crypto_inst.Expiry > @T_DATE OR cme_inst.Expiration > @T_DATE OR (crypto_inst.Expiry is null and cme_inst.Expiration is null))
+	 GROUP BY YYYYMMDD, endpoint, Client, Account, Symbol, crypto_inst.Expiry, cme_inst.Expiration, crypto_inst.rootsymbol, cme_inst.currency_position, crypto_inst.IsInverse, 
+		crypto_inst.currency_position, crypto_inst.Inst_Type, cme_inst.SecurityType, marks.closingprice, txn_count,	today_short_quantity, today_long_quantity
+	 HAVING 
+		NOT(abs(sum(CASE WHEN [TYPE] = 'BALANCE' THEN value ELSE 0 END)) <= 1e-08)
+	 ORDER BY YYYYMMDD, Client, endpoint, Account, Symbol
 """
 
 
@@ -196,12 +194,18 @@ class TradingRepository:
             db_connector_factory=self._db_connector_factory).as_list()
         return data[0][0].date()
 
+    def get_bank_balances(self, eod_date: dt.date) -> SqlQueryInterface:
+        return self._sql_query_class(
+            GET_CRYPTO_BANKBALANCES.format(eod_date=eod_date),
+            db_connector_factory=self._db_connector_factory)
+
     def get_cme_positions(self, at_dtt_utc: dt.datetime) -> SqlQueryInterface:
         return self._sql_query_class(
             GET_CME_POSITIONS.format(accounts=('UNMBF222', ''), as_of=at_dtt_utc.astimezone(ChicagoTimeZone)),
             db_connector_factory=self._db_connector_factory)
 
     def get_operations_eod_balances(self, eod_date: dt.date) -> SqlQueryInterface:
+        # Todo: remove this & use OperationsRepository.get_eod_balances
         return self._sql_query_class(
             GET_OPERATIONS_EOD_BALANCES.format(eod_date=eod_date),
             db_connector_factory=self._db_connector_factory)
@@ -214,7 +218,10 @@ class TradingRepository:
 
 
 GET_RAWDATA_EXCHANGE_BALANCES = """
-SELECT * FROM (
+SELECT 
+    Account, balance as Balance, BalanceType, UPPER(currency) as Currency,
+    CONCAT('RawData.', endpoint) as Source, CONVERT(DATE, asof) as Timestamp, asof as Timestamp_Native
+FROM (
 /**/  SELECT asset as currency, 'BINE' as endpoint, 'BINE-1-M-M' AS Account, '' AS BalanceType, netAsset as balance, timestamp as asof, ROW_NUMBER() OVER (PARTITION BY asset ORDER BY Timestamp DESC) as rn 
   from RawData.bine_1_m_m.margin_account_asset WITH(NOLOCK) WHERE timestamp BETWEEN '{from_ts:%Y-%m-%d %H:%M:%S}' AND '{to_ts:%Y-%m-%d %H:%M:%S}' 
   UNION
